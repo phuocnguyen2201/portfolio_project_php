beb5ec7f9ced2a44e9be3a2e8bfbd98a57348d64
phuocng <phuocnguyen2201@gmail.com>
2024-02-04 23:15:34 +0200

    Update uploadfile for modify page

--- modify.php
+++ modify.php
@@ -12,13 +12,52 @@
     include "extention/header.php"?>
     <div class="content-white py-5">
         <div class="container-lg">
-            <?php
-                $receivedData = $_GET['username'];
-                
-                ?>
+
         <div class="row">
                 <div class="col-lg-4 text-center">
+                <?php
+                $receivedData = $_GET['username'];
+                if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['formFile'])){
+
+                    $file = $_FILES['file'];
+                    
+                    $fileName       = $_FILES['file']['name'];
+                    
+                    $fileTmpName    = $_FILES['file']['tmp_name'];
+                    $fileSize       = $_FILES['file']['size'];
+                    $fileError      = $_FILES['file']['error'];
+                    $fileType       = $_FILES['file']['type'];
+
+                    $fileExt = explode('.', $fileName);
+                    $fileActualExt = strtolower(end($fileExt));
+
+                    $allowed = array('jpg', 'jpeg', 'png');
+
+                    if(in_array($fileActualExt, $allowed)){
+                        if($fileError === 0){
+                            if($fileSize < 10000000){
+                                $fileNameNew = "profile".$receivedData.".".$fileActualExt;
+                                $fileDestination = 'image/teams/'.$fileNameNew;
+                                move_uploaded_file($fileTmpName, $fileDestination);
+                                //header("Location: modify.php?uploadsuccess");
+                                echo "<img class='img-thumbnail' alt='".$file."'s portrait' src='image/teams/".$fileTmpName."'>";
+                            } else {
+                                echo "Your file is too big!";
+                            }
+                        } else {
+                            echo "There was an error uploading your file!";
+                        }
+                    } else {
+                        echo "You cannot upload files of this type!";
+                    }
+                }
+                ?>
                     <img class="img-thumbnail" alt="Phuoc's portrait" src="image/teams/phuoc.jpg">
+                    <form name="formFile" method="post" enctype="multipart/form-data">
+                        <label for="formFile" class="form-label">Image</label>
+                        <input class="form-control form-control-lg" name='file' id="formFile" type="file" accept=".jpg,.jpeg,.png">
+                        <input class='btn-bottom' type='submit' name='formFile' value='Save'>
+                    </form>
                 </div>
                 <div class="col-lg-8">
                     <div class="row">
@@ -65,7 +104,7 @@
                         <br>
                         <div class='form-group row' style='text-align: left;'>
                             <div class='col-sm-3'></div>
-                            <div class='col-sm-8'><input class='btn-bottom' type='submit' name='form-login' value='Save'></div>
+                            <div class='col-sm-8'><input class='btn-bottom' type='submit' name='form-edit-information' value='Save'></div>
                         </div>
                     </form>
                     </div>
